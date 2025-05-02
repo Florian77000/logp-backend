342f3b94a5ac7b78b5a9fa704d643557cc6aaca1
Florian <floriandelaunay75@gmail.com>
2025-05-02 19:28:00 +0200

    ajout de la route pour afficher tous les users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require("../models/connection");
+const User = require("../models/users");
+
+router.get("/", (req, res) => {
+  User.find().then((data) => {
+    res.json({ users: data });
+  });
 });
 
 module.exports = router;
